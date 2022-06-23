b6cb4b1c5c86017d831e1c131eb2219c6295a410
Ebuka Ejiofor <ebuskelele@gmail.com>
2022-06-23 11:28:41 +0100

    Add comments to JavaScript code

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+// How to ise message
 alert("This is a simple calculator made with JavaScript \n How to use:\n 1. Enter a number \n 2. Select a math operator \n 3. Enter a second number \n 4. ...then boom you have you Result! \n Press 'OK' to continue.")
 
+// Declared variables
 var firstNumber = prompt("Enter a number")
 
 var mathOpertor = prompt("Select an operator \n 1. Add \n 2. Subtract \n 3. Divide \n 4. Multiply")
 
 var secondNumber = prompt("Enter a second number")
 
+// Show result of the selected operation
 if(mathOpertor == 1) {
   alert("Result: " + Number(firstNumber) + Number(secondNumber))
 }
